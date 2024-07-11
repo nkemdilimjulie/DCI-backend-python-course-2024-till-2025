74636e058f5bf00e252f77ab3dbd6ef566757f8b
kamrujjaman Md <kamrujjaman.md@digitalcareerinstitute.org>
2024-07-11 12:09:02 +0200

    Add lesson files

---
+++ fundamentals/lesson-2024-07-11/README.md
@@ -0,0 +1,21 @@
+## Yesterday - 2024-07-10
+
+* Programming
+* Types of Programming languages
+* Algorithms and flowchart
+* Tooling (VS code)
+* Virtual environment (pyenv)
+* Python Hello world
+* Exercise
+
+
+## Today - 2024-07-11
+
+* Python module
+* print
+* Variables and Data types
+* Input
+* Operators
+* math functions
+* one file python program
+* Exercise
---
+++ fundamentals/lesson-2024-07-11/introduction.py
@@ -0,0 +1,90 @@
+
+# print('Hello Python!', 'Welcome to Python module.', 2024, sep=' ', end='\n')
+
+
+name = 'Roger Federer'
+
+age_1 = 34
+
+Age_2 = 42
+
+age_3 = 50
+
+"""
+print(name)
+print(age_1)
+print(Age_2)
+"""
+
+'''This is a comment
+This is multi line comment
+Comments are continuing'''
+
+"""
+This is a comment
+This is multi line comment
+Comments are continuing
+These are called docstrings
+"""
+
+
+# camel case naming
+personAgeHeight = '28 and 172 cm'
+
+# pascal case naming
+PersonAgeHeight = '28 and 172 cm'
+
+# snake case naming
+person_age_height = '28 and 172 cm' # This is a line comment
+
+# print(person_age_height)
+
+
+# x, y, z = 'green', 'blue', 'yellow'
+
+# print(y)
+
+
+x = y = z = 'magenta'
+
+# print(z)
+# print(x)
+
+
+color_1 = 'green'
+color_2 = 'purple'
+# print(color_2)
+
+# swap
+color_1, color_2 = color_2, color_1
+
+# print(color_2)
+
+
+
+# Data types in Python
+
+
+
+"""
+string, integer, float, boolean, complex, None 
+"""
+
+first_name = 'Thomas'
+
+age = 22
+
+weight = 65.7
+
+is_student = True
+
+complex_num = 5+7j
+
+temperature = None
+
+
+print(type(first_name))
+print(type(weight))
+print(type(temperature))
+
+
