4c9723303931966bb11573b002b67ccf9c333f26
kamrujjaman Md <kamrujjaman.md@digitalcareerinstitute.org>
2024-07-11 14:43:15 +0200

    Add lesson files

--- fundamentals/lesson-2024-07-11/introduction.py
+++ fundamentals/lesson-2024-07-11/introduction.py
@@ -22,7 +22,7 @@ Comments are continuing'''
 
 """
 This is a comment
-This is multi line comment
+This is multi-line comment
 Comments are continuing
 These are called docstrings
 """
@@ -83,8 +83,112 @@ complex_num = 5+7j
 temperature = None
 
 
-print(type(first_name))
-print(type(weight))
-print(type(temperature))
+# print(isinstance(age, int))
+
+# print(isinstance(complex_num, complex))
 
 
+# print('Is age an integer?:', isinstance(age, int))
+
+
+
+
+# print(type(first_name))
+# print(type(weight))
+# print(type(temperature))
+
+
+
+# user input
+
+# sport = input('What is your favorite sport? ')
+
+# print('I see, your favorite sport is', sport)
+
+# print(type(sport))
+
+
+# number = int(input('Enter a number: '))
+
+# real_number = int(number)
+
+# print(type(number))
+
+
+# Math operators
+
+
+"""addition(+), subtraction(-), multiplication(*), division(/), modulus(%), floor division(//)"""
+
+
+# num1 = 120
+# num2 = 80
+
+# result = num1 / num2
+
+# print(result)
+
+
+# complex_number = <real_part> + <complex_part>j
+
+# num1 = 4+7j
+# num2 = 3+2j
+
+# result = num1 - num2
+
+# print(result)
+
+
+
+num1 = 20
+num2 = 6
+
+# result = num1 % num2
+
+result = num1 // num2
+
+# print(result)
+
+
+
+maximum_num = max(44, 35, 127, 500, 3, 77, 222)
+
+minimum_num = min(44, -35, 127, 500, 3, -77, 222)
+
+# print(minimum_num)
+
+
+# number = -2550
+
+# abs_num = abs(4-3j) # -20
+
+# print(abs_num)
+
+
+
+# number = round(12.8527845, 2)
+
+# print(number)
+
+# number = pow(3, 7) # 3^3 = 3 * 3 * 3 * 3 * 3 * 3 * 3
+
+# print(number)
+
+
+import math
+
+# number = math.pi
+
+# print(number)
+
+
+# number = math.sqrt(49)
+
+# print(number)
+
+
+# number = math.ceil(435.0375)
+
+number = math.floor(10.45)
+
+print(number)
