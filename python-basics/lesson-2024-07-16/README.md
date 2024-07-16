2b27186cef7c5c7ea7f0ffca74572e67b8f404c9
kamrujjaman Md <kamrujjaman.md@digitalcareerinstitute.org>
2024-07-16 12:20:35 +0200

    Add lesson files

---
+++ python-basics/lesson-2024-07-16/README.md
@@ -0,0 +1,29 @@
+## Friday - 2024-07-12
+
+* Bits and Bytes
+
+* Number systems
+
+* Relational/Comparison Operators
+
+* Boolean and Logical Operators
+
+
+## Today - 2024-07-16
+
+* if...elif..else
+
+* Indentation
+
+* for loop
+
+* while loop
+
+* continue, break
+
+* nested loop
+
+* function 
+
+* Exercise
+
---
+++ python-basics/lesson-2024-07-16/condition.py
@@ -0,0 +1,151 @@
+
+# if..else
+
+"""
+if condition:
+    code block to be executed if the condition is true.
+else:
+    code block to be executed if the condition is false.
+
+"""
+
+x = 100
+
+"""if x < 100:
+    print('x is less than 100.')
+elif x == 100:
+    print('x is equal to 100.')
+else:
+    print('x is greater than 100')"""
+
+
+# x = 3040
+
+# if x < 100:
+#     y = 50
+#     z = x + y
+#     print(z)
+#     print('x is less than 100.')
+# elif x == 100:
+#     print('Just trying out.')
+#     print('x is equal to 100.')
+# else:
+#     print('This is the condition x falls into.')
+#     print('Hello World')
+#     print('x is greater than 100')
+
+
+
+# for loop
+
+'''
+for item in sequence:
+    code to be executed
+
+'''
+
+
+'''
+for item in 'Good Morning, Today is Tuesday!':
+    print(item)
+'''
+
+
+# for item in 'Good Morning, Today is Tuesday':
+#     print(item, end='\n')
+
+# print(item)
+# print(item)
+# print(item)
+
+
+
+range(10)
+
+'''
+0, 1, 2, 3,........8, 9
+'''
+
+# print(range(5, 10))
+
+
+# for i in range(5, 10):
+#     i = i + 100
+#     if i % 2 == 1:
+#         print(i)
+
+
+# for i in range(0, 10):
+#     if i % 2 == 1:
+#         print(i)
+
+
+
+# while loop
+
+
+"""
+while True:
+    code to be executed.
+    (condition to exit out)
+    
+"""
+
+
+'''
+x = 1
+
+while x <= 10:
+    if x % 5 == 0:
+        print(x)
+    # x = x + 1
+    x += 1
+    
+'''
+
+    
+
+
+'''
+x = 1
+print(x)
+while x <= 9:  
+    x += 1
+    if x == 5:
+        break
+    print(x)
+'''
+
+
+
+# for i in range(10):
+#     if i == 7:
+#         continue
+#     print(i)
+
+
+# for i in range(10):
+#     if i == 7:
+#         break
+#     print(i)
+#     print('Lunch time')
+
+
+# for i in range(10):
+#     if i == 7 or i == 8 or i == 3:
+#         continue
+#     print(i)
+
+
+# for i in range(10):
+#     if i in [3, 7, 8]:
+#         continue
+#     print(i)
+
+
+
+for i in range(3):
+    for j in range(4):
+        print(i, j)
+
+
