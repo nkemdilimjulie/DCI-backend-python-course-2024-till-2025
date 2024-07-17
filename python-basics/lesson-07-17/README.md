f86eae5e271093df819dcf33598bda1c30c1385a
kamrujjaman Md <kamrujjaman.md@digitalcareerinstitute.org>
2024-07-17 12:13:30 +0200

    Add lesson files

---
+++ python-basics/lesson-07-17/README.md
@@ -0,0 +1,30 @@
+
+## Yesterday - 2024-07-16
+
+* if...elif..elif..elif..elif..else
+
+* Indentation
+
+* for loop
+
+* while loop
+
+* continue, break
+
+* nested loop
+
+* Exercise
+
+
+## Today - 2024-07-17
+
+* String
+* String vs character
+* String method
+* Exercises
+
+hell\+o = hell+o
+
+'world'
+
+
---
+++ python-basics/lesson-07-17/texts.py
@@ -0,0 +1,116 @@
+
+# String and character
+
+
+text = 'hello world, we are learning about texts in Python.'
+
+# print(text)
+
+# new_text = text.capitalize()
+
+# new_text = text.title()
+
+# new_text = text.upper()
+
+# new_text = text.lower()
+
+
+# print(new_text)
+
+
+# name = 'ThOmaS'
+
+# print(name.lower() == 'thomas muller')
+
+
+# name = 'Monalisa'
+
+# print(name.isalpha())
+
+# num = '4312078'
+
+# print(num.isdecimal())
+
+
+
+# num = '4312078th'
+
+# print(num.isnumeric())
+
+# num = '453490'
+
+# num = '\u0030' # 0 u=unicode
+
+# num = '\u2165' # 1/2
+
+# print(num.isnumeric())
+
+
+# print(num.isdecimal())
+
+
+# name = 'Monalisa33'
+
+# print(name.isalnum())
+
+
+
+# name = 'Monalisa is a famous painting.'
+
+# print(name.find('beautiful'))
+
+
+# name = 'Monalisa is a famous painting.'
+
+# print(name.index('famous'))
+
+
+
+# text = 'Monalisa is a famous painting. It is dislayed on a famous meusium in Paris famous.'
+
+
+# newText = text.replace('famous', 'beautiful', 1)
+
+# print(newText)
+
+
+
+# text = 'Monalisa is a famous painting. It is dislayed on a famous meusium in Paris.'
+
+
+# words = text.split(sep='famous', maxsplit=1)
+
+
+# print(words)
+
+
+# text_list = ['Monalisa', 'is', 'a', 'famous', 'painting.', 'It', 'is', 'dislayed', 'on', 'a', 'famous', 'meusium', 'in', 'Paris', 'famous.']
+
+
+# text = ' '.join(text_list)
+
+
+# print(text)
+
+
+
+# text = 'Monalisa is a famous painting. It is dislayed on a famous meusium in Paris.'
+
+name = 'Monalisa'
+
+
+single_char = name[5]
+
+# print(single_char)
+
+# sub_name = name[4:]
+
+# print(sub_name)
+
+# count = len(name)
+
+# print(count)
+
+# print(name[::-1])
+
+print(name[-1])
